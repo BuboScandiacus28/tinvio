29461ad231ab8c9f8b8e29c6dab87e4216fff866
BuboScandiacus28 <28filosof28@gmail.com>
2020-12-25 00:07:15 +0200

    added section 'product present' v 1

--- js/common.js
+++ js/common.js
@@ -1,3 +1,19 @@
 $('.owl-carousel').owlCarousel({
     items: 1,
+    mouseDrag: false,
+    nav: true,
 })
+
+$('.owl-nav button.owl-prev')[0].addEventListener('click', () => {
+    let id = $('.product-present__description.active')[0].getAttribute('id').split('product-present__description-')[1];
+    if (id == 1) return;
+    document.querySelector(`#product-present__description-${id}`).classList.toggle('active');
+    document.querySelector(`#product-present__description-${--id}`).classList.toggle('active');
+});
+
+$('.owl-nav button.owl-next')[0].addEventListener('click', () => {
+    let id = $('.product-present__description.active')[0].getAttribute('id').split('product-present__description-')[1];
+    if (id == 3) return;
+    document.querySelector(`#product-present__description-${id}`).classList.toggle('active');
+    document.querySelector(`#product-present__description-${++id}`).classList.toggle('active');
+});
